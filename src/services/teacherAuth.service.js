3a42d2f939077ff66d7bd34a8f1fce83b23ad3a1
sr-roboto <marcoslopez123.lu@gmail.com>
2024-09-13 16:29:35 -0300

    Documentar la función de registro en teacherAuth.services.js

--- src/services/teacherAuth.service.js
+++ src/services/teacherAuth.service.js
@@ -5,20 +5,41 @@ import { teacherModel } from '../models/teacher.model.js';
 import { createAccessToken } from '../libs/jwt.js';
 import { JWT_SECRET } from '../configs/envConfig.js';
 
+/**
+ * Registro de un nuevo profesor.
+ *
+ * @author sr-roboto
+ * @param {Object} teacherData - La Data del profesor que va a ser registrado.
+ * @param {string} teacherData.email - El email del profesor.
+ * @param {string} teacherData.password - La contraseña del profesor.
+ * @returns {Object} - Devuelve un objeto que contiene los datos del profesor y el token de acceso.
+ * @throws {Error} - Muesta un error si el email ya esta registrado.
+ */
+
 const registerTeacherService = async (teacherData) => {
   const { email, password } = teacherData;
+
+  // Verificar si el profesor ya está registrado
   const existingTeacher = await teacherModel.findOne({ email });
 
+  // Si el profesor ya está registrado, lanzar un error
   if (existingTeacher) {
     throw new Error('El correo ya está registrado');
   }
 
+  // Encriptar la contraseña
   const hashedPassword = await bcrypt.hash(password, 12);
+
+  // Crear un nuevo profesor con la contraseña encriptada
   const newTeacher = new teacherModel({
     ...teacherData,
     password: hashedPassword,
   });
+
+  // Guardar el profesor en la base de datos
   const savedTeacher = await newTeacher.save();
+
+  // Crear un token de acceso con el id del profesor
   const token = await createAccessToken({ id: savedTeacher._id });
 
   return { savedTeacher, token };
