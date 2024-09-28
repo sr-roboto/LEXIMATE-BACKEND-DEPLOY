de87db8722dc5bae876978d5605f23c2db0b855d
sr-roboto <marcoslopez123.lu@gmail.com>
2024-09-28 14:10:52 -0300

    Refactorizar nombres de permisos y descripciones en el modelo Permission

--- src/models/permission.model.js
+++ src/models/permission.model.js
@@ -31,26 +31,22 @@ const definePermissions = async () => {
     const permissions = await Permission.findAll();
     if (permissions.length === 0) {
       await Permission.bulkCreate([
-        { name: 'create:classes', description: 'Crear clases.' },
-        { name: 'read:classes', description: 'Leer clases.' },
-        { name: 'update:classes', description: 'Actualizar clases.' },
-        { name: 'delete:classes', description: 'Eliminar clases.' },
-        { name: 'create:tasks', description: 'Crear tareas.' },
-        { name: 'read:tasks', description: 'Leer tareas.' },
-        { name: 'update:tasks', description: 'Actualizar tareas.' },
-        { name: 'delete:tasks', description: 'Eliminar tareas.' },
-        { name: 'create:users', description: 'Crear usuarios.' },
-        { name: 'read:users', description: 'Leer usuarios.' },
-        { name: 'update:users', description: 'Actualizar usuarios.' },
-        { name: 'delete:users', description: 'Eliminar usuarios.' },
-        { name: 'create:roles', description: 'Crear roles.' },
-        { name: 'read:roles', description: 'Leer roles.' },
-        { name: 'update:roles', description: 'Actualizar roles.' },
-        { name: 'delete:roles', description: 'Eliminar roles.' },
-        { name: 'create:permissions', description: 'Crear permisos.' },
-        { name: 'read:permissions', description: 'Leer permisos.' },
-        { name: 'update:permissions', description: 'Actualizar permisos.' },
-        { name: 'delete:permissions', description: 'Eliminar permisos.' },
+        {
+          name: 'Create',
+          description: 'Crear un registro.',
+        },
+        {
+          name: 'Read',
+          description: 'Leer un registro.',
+        },
+        {
+          name: 'Update',
+          description: 'Actualizar un registro.',
+        },
+        {
+          name: 'Delete',
+          description: 'Eliminar un registro.',
+        },
       ]);
       console.log('Permisos creados correctamente.');
     }
