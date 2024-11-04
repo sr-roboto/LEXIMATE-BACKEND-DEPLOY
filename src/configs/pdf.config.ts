11ba8f94ac8c8aafb599389ce8343ce90f717847
sr-roboto <marcoslopez123.lu@gmail.com>
2024-11-04 14:09:07 -0300

    Actualizar configuración de carga de archivos: aumentar límite de tamaño a 10 MB; refactorizar controlador para extraer texto solo de PDF; simplificar validaciones y manejo de errores; agregar configuración para API de extracción de texto

---
+++ src/configs/pdf.config.ts
@@ -0,0 +1,10 @@
+import { PDFREST_API_KEY } from './env.config';
+
+const PDFREST_URL = 'https://api.pdfrest.com';
+
+const PDF_CONFIG = {
+  url: PDFREST_URL,
+  apiKey: PDFREST_API_KEY,
+};
+
+export { PDF_CONFIG };
--- src/services/userTool.service.ts
+++ src/services/userTool.service.ts
@@ -1,18 +1,32 @@
-import pdf from 'pdf-parse';
-import mammoth from 'mammoth';
+// import pdf from 'pdf-parse';
+import { PDF_CONFIG } from '../configs/pdf.config';
 
 const extractTextFromPdfService = async (
   pdfBuffer: Buffer
 ): Promise<string[]> => {
   try {
-    const data = await pdf(pdfBuffer);
+    const formData = new FormData();
+    formData.append(
+      'file',
+      new Blob([pdfBuffer], { type: 'application/pdf' }),
+      'file.pdf'
+    );
+    formData.append('word_style', 'on');
+    formData.append('output_type', 'json');
 
-    let text = data.text;
-    text = text.replace(/\n/g, '  '); // Usar expresión regular global
+    const response = await fetch(`${PDF_CONFIG.url}/extracted-text`, {
+      method: 'POST',
+      headers: {
+        'Api-Key': PDF_CONFIG.apiKey,
+      },
+      body: formData,
+    });
 
-    const textArray = text.split('  ');
-
-    return textArray; // Retorna el texto extraído
+    if (!response.ok) {
+      throw new Error(`Error: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
   } catch (error) {
     if (error instanceof Error) {
       throw new Error(error.message);
@@ -22,25 +36,25 @@ const extractTextFromPdfService = async (
   }
 };
 
-const extractTextFromDocxService = async (
-  docxBuffer: Buffer
-): Promise<string[]> => {
-  try {
-    const data = await mammoth.extractRawText({ buffer: docxBuffer });
+// const extractTextFromDocxService = async (
+//   docxBuffer: Buffer
+// ): Promise<string[]> => {
+//   try {
+//     const data = await mammoth.extractRawText({ buffer: docxBuffer });
 
-    let text = data.value;
-    text = text.replace(/\n/g, '  '); // Usar expresión regular global
+//     let text = data.value;
+//     text = text.replace(/\n/g, '  '); // Usar expresión regular global
 
-    const textArray = text.split('  ');
+//     const textArray = text.split('  ');
 
-    return textArray; // Retorna el texto extraído
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(error.message);
-    } else {
-      throw new Error('Error desconocido al extraer texto del archivo DOCX');
-    }
-  }
-};
+//     return textArray; // Retorna el texto extraído
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       throw new Error(error.message);
+//     } else {
+//       throw new Error('Error desconocido al extraer texto del archivo DOCX');
+//     }
+//   }
+// };
 
-export { extractTextFromPdfService, extractTextFromDocxService };
+export { extractTextFromPdfService };
